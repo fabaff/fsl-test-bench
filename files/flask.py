4e1344f96b3f5791fbfb124c0b4131625d8865da
Fabian Affolter <fabian@affolter-engineering.ch>
2020-05-25 13:05:19 +0200

    Use new Python 3.x feature and add docstring

--- files/flask.py
+++ files/flask.py
@@ -1,18 +1,22 @@
-from flask import Flask
-from flask import render_template
-
+"""Simple Flask application."""
 import time
-#import pylinux.pylinux as pylinux
+
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!' + 'Time now ' + time.ctime()
+    """Default route for the server."""
+    return f"Hello World! Time: {time.ctime()}"
+
 
 @app.route("/uptime")
 def uptime():
-    return 'Test bench up for %s hours' % time.ctime() #str(pylinux.uptime())
+    """Route for the server's uptime."""
+    return f"Test bench up for {time.ctime()} hours"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
